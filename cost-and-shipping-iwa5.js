8ccbfd9d998254e1557bc35e21904b4cba027271
seanc0der <sean.noko@gmail.com>
2023-08-08 00:11:18 +0200

    Updated the .js file codebase by fixing syntax and amending and adding code.

--- cost-and-shipping-iwa5.js
+++ cost-and-shipping-iwa5.js
@@ -1,34 +1,82 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = "Free shipping only applies to single customer orders";
+const BANNED_WARNING =
+	"Unfortunately we do not ship to your country of residence";
+const NONE_SELECTED = 0;
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
-
-if location = NAM
-shipping = 600 
-else shipping = 800
-
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
+const order_info = {
+	customers: 1,
+	location: "RSA",
+	item_units: {
+		shoes: 1,
+		toys: 5,
+		shirts: NONE_SELECTED,
+		batteries: 2,
+		pens: NONE_SELECTED,
+	},
+	item_cost: {
+		shoes: 300,
+		toys: 100,
+		shirts: 150,
+		batteries: 35,
+		pens: 5,
+	},
+	total_cost: function () {
+		total = 0;
+		for (let item_name in this.item_units) {
+			if (item_name in this.item_cost) {
+				total += this.item_cost[item_name] * this.item_units[item_name];
+			}
 		}
+		return total;
+	},
+};
+
+total_cost_and_shipping = function (cust_count, country, total_cost) {
+	let currency = "";
+	let final_price = 0;
+	const shipping_amount = {
+		rsa: 400,
+		nam: 600,
+		other_countries: 800,
+	};
+
+	if (country === "RSA") {
+		currency = "R";
+
+		if (total_cost >= 1000 && cust_count === 1) {
+			// eligible for free shipping
+			final_price = total_cost;
+		} else if (total_cost >= 1000 && cust_count !== 1) {
+			return FREE_WARNING;
+		} else {
+			final_price = total_cost + shipping_amount.rsa;
+		}
+	} else if (country === "NAM") {
+		currency = "$";
+
+		if (total_cost >= 60 && cust_count === 1) {
+			// eligible for free shipping
+			final_price = total_cost;
+		} else if (total_cost >= 1000 && cust_count !== 1) {
+			return FREE_WARNING;
+		} else {
+			final_price = total_cost + shipping_amount.nam;
+		}
+	} else if (country !== "NK") {
+		currency = "$";
+		final_price = total_cost + shipping_amount.other_countries;
+	} else {
+		// North Korea (NK) not eligible for shipping
+		return BANNED_WARNING;
 	}
-}
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+	return "Price: " + currency + final_price.toString();
+};
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
-currency = null
+console.log(
+	total_cost_and_shipping(
+		order_info.customers,
+		order_info.location,
+		order_info.total_cost()
+	)
+);
